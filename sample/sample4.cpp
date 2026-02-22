626890aee2981f16b42625cf6ebf6b86e709731f
tatenami <ttnm139th@gmail.com>
2026-02-22 19:37:41 +0900

    Update sample

--- sample/sample4.cpp
+++ sample/sample4.cpp
@@ -1,6 +1,5 @@
 #include <cstdio>
 #include "../tts.hpp"
-#include <chrono>
 
 using namespace tts;
 using namespace std;
@@ -9,13 +8,7 @@ Task task1() {
 
   for (int i = 1; i <= 3; i++) { 
     printf("[task1] num: %d\n", i);
-
-    chrono::time_point start = chrono::high_resolution_clock::now();
     co_await sleep_ms(5);
-    chrono::time_point ts = chrono::high_resolution_clock::now();
-
-    int delay_us = chrono::duration_cast<chrono::microseconds>(ts - start).count();
-    printf("[task1] delay=%d(us)\n", delay_us);
   }
 }
 
@@ -23,13 +16,7 @@ Task task2() {
 
   for (int i = 1; i <= 3; i++) {
     printf("[task2] num: %d\n", i);
-
-    chrono::time_point start = chrono::high_resolution_clock::now();
     co_await sleep_ms(2);
-    chrono::time_point ts = chrono::high_resolution_clock::now();
-
-    int delay_us = chrono::duration_cast<chrono::microseconds>(ts - start).count();
-    printf("[task2] delay=%d(us)\n", delay_us);
   }
 }
 
